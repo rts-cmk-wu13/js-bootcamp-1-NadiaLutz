26d205b4004a15c7e57bf537bc7c2f4f6a6ca7f6
Nadia Lutz <na.lutz@outlook.com>
2025-01-28 10:03:48 +0100

    added JS array with quotes

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let quotes = ["And those who were seen dancing were thought to be insane by those who could not hear the music.", "Whoever fights monsters should see to it that in the process he does not become a monster. And if you gaze long enough into an abyss, the abyss will gaze back into you.", "There are no facts, only interpretations.", "Without music, life would be a mistake.", "I'm not upset that you lied to me, I'm upset that from now on I can't believe you.", "You must have chaos within you to give birth to a dancing star."];
-let quotesListElm = document.querySelector(".catslist")
+let quotesListElm = document.querySelector(".quotelist")
 
 let i = 0;
+
+quotes.forEach(function(quote) {
+    quotesListElm.innerHTML += `<blockquote>${quote}</blockquote>`;
+    quotes.random();
+    })
